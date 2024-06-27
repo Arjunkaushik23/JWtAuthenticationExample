49889972d38387fef6449a501f41111803bcb352
Arjun Kaushik <88938626+Arjunkaushik23@users.noreply.github.com>
2024-06-27 16:23:22 +0530

    remove a new line

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -51,8 +51,6 @@ public class UserController {
     public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
         if (authentication.isAuthenticated()) {
-            System.out.println("Hello This is new change");
-            System.out.println("My changes are not reflecting");
             return jwtService.generateToken(authRequest.getUsername());
         } else {
             throw new UsernameNotFoundException("invalid user request !");
